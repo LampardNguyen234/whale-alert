0a9baef67991aeb6a722e599c780d57dfb4fa336
thanhnv <thanhnv@stellalab.com>
2023-06-16 09:15:49 +0700

    refactor: change db structure

--- internal/api/handler_auth.go
+++ internal/api/handler_auth.go
@@ -13,24 +13,26 @@ const authKey = "097a5daf05c90ed9105d983f74346c01e1ab5e2199064266b31b742b71303db
 // APIAuthenticateHandler is a middleware for authenticating the request based on ApiKey.
 func (s *HTTPServer) APIAuthenticateHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var token struct {
-			Authorization string `json:"Authorization" binding:"required"`
-		}
+		if !strings.Contains(c.ClientIP(), "127.0.0.1") && !strings.Contains(c.ClientIP(), "::1") {
+			var token struct {
+				Authorization string `json:"Authorization" binding:"required"`
+			}
 
-		err := c.ShouldBindHeader(&token)
-		if err != nil {
-			s.log.Errorf("Invalid header: %v", err)
-			unauthorized(c, http.StatusUnauthorized, err.Error())
-			return
-		}
-		s.log.Infof("New request from %v, path %v", c.ClientIP(), c.Request.URL.Path)
+			err := c.ShouldBindHeader(&token)
+			if err != nil {
+				s.log.Errorf("Invalid header: %v", err)
+				unauthorized(c, http.StatusUnauthorized, err.Error())
+				return
+			}
+			s.log.Infof("New request from %v, path %v", c.ClientIP(), c.Request.URL.Path)
 
-		if strings.ToLower(
-			fmt.Sprintf("%x", sha256.Sum256([]byte(token.Authorization))),
-		) != authKey { // only basic authentication =)) HackMeIfYouCan
-			s.log.Errorf("Invalid token: %v", token.Authorization)
-			unauthorized(c, http.StatusUnauthorized, err.Error())
-			return
+			if strings.ToLower(
+				fmt.Sprintf("%x", sha256.Sum256([]byte(token.Authorization))),
+			) != authKey { // only basic authentication =)) HackMeIfYouCan
+				s.log.Errorf("Invalid token: %v", token.Authorization)
+				unauthorized(c, http.StatusUnauthorized, err.Error())
+				return
+			}
 		}
 
 		c.Next()
